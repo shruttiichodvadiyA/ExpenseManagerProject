5559fb9767173c8ac4b98ac011061320b0bb2a38
shruttiichodvadiyA <121149502+shruttiichodvadiyA@users.noreply.github.com>
2023-05-15 08:40:28 +0530

    version Update

--- app/src/main/java/com/example/expensemanagerproject/Splash_Screen.kt
+++ app/src/main/java/com/example/expensemanagerproject/Splash_Screen.kt
@@ -1,6 +1,7 @@
 package com.example.expensemanagerproject
 
 import android.content.Intent
+import android.content.pm.PackageInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -19,5 +20,13 @@ class Splash_Screen : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, 1000)
+
+        //version
+        val manager = packageManager
+        val info: PackageInfo = manager.getPackageInfo(
+            packageName, 0
+        )
+        val version: String = info.versionName
+        binding.txtversion.text = version
     }
 }
